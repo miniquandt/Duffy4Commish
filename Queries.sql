1fe462cb0d5bf0079dc2db16ebefb4f32faef902
Andrew Quandt <79469121+aquandt-basis@users.noreply.github.com>
2023-09-23 02:28:07 -0400

    fix query

--- Queries.sql
+++ Queries.sql
@@ -1,3 +1,4 @@
-SELECT l.id, l.name, l.slug, t.name i.id FROM leagues l INNER JOIN tournaments t on t.leagueid = l.id WHERE l.slug = "lcs";
+SELECT l.id, l.name, l.slug, t.name, t.id FROM leagues l 
+INNER JOIN tournaments t on t.leagueid = l.id WHERE l.slug = "lcs";
 
 SELECT * FROM teams WHERE slug = "cloud9";
