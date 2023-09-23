baeb4cfdfc11bb69e0b5277d8dfcf7efb1c00274
Andrew Quandt <79469121+aquandt-basis@users.noreply.github.com>
2023-09-23 05:21:13 -0400

    more queries

--- Queries.sql
+++ Queries.sql
@@ -1,4 +1,17 @@
 SELECT l.id, l.name, l.slug, t.name, t.id FROM leagues l 
-INNER JOIN tournaments t on t.leagueid = l.id WHERE l.slug = "lcs";
+INNER JOIN tournaments t on t.leagueid = l.id WHERE l.slug = "lcs" LIMIT 1;
 
 SELECT * FROM teams WHERE slug = "cloud9";
+
+/* Gets teams total gold*/
+SELECT eventtime, eventtype, platformgameid, teams.totalgold, gametime
+FROM games 
+WHERE eventtype = "stats_update" 
+LIMIT 10;
+
+
+/* Gets teams total gold at a specific time (14 minutes= 840 seconds=840000ms*/
+SELECT eventtime, eventtype, platformgameid, teams.totalgold, gametime
+FROM games 
+WHERE eventtype = "stats_update" AND gametime > 840000
+LIMIT 10;
