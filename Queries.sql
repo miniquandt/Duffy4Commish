f3cb0890c01420830452318fca20e1aff18f4823
Andrew Quandt <79469121+aquandt-basis@users.noreply.github.com>
2023-10-09 06:24:20 -0400

    f riot

--- Queries.sql
+++ Queries.sql
@@ -16,10 +16,12 @@ WHERE eventtype = "stats_update" AND gametime > 840000
 LIMIT 10;
 
 /* Gets Game IDs for all matches for a specific tournament */
-SELECT t.id AS Tournament_Id, t.slug AS Tournament_Slug, et.name AS Match_Name, et2.id AS Game_Ids
+SELECT t.id AS Tournament_Id, t.slug AS Tournament_Slug, et.name AS Match_Name, et2.id AS Matches_Ids, et3.id AS Game_Ids
 FROM tournaments t 
 LATERAL VIEW explode(stages.sections) exploaded_table AS et
-LATERAL VIEW explode(et.matches) exptab2 AS et2;
+LATERAL VIEW explode(et.matches) exptab2 AS et2
+LATERAL VIEW explode(et2.games) exptab3 AS et3
+WHERE t.slug like "lcs_s%" OR t.slug like "lck_s%" or t.slug like "lec_s%";
 /*WHERE slug = 'lcs_summer_2022';*/
 
 /**********************************************************************************
@@ -38,3 +40,12 @@ SELECT t.id, et.name FROM tournaments t LATERAL VIEW explode(stages.sections) ex
 SELECT t.id, et.matches FROM tournaments t LATERAL VIEW explode(stages.sections) exploaded_table AS et WHERE t.slug = 'lcs_summer_2022';
 
 LATERAL VIEW explode(et.matches) exptab2 AS et2 WHERE t.slug = 'lcs_summer_2022';
+
+/*//////////////////////////////////////////////////////////////////////////// */
+/* Creates new table with way less info for tournaments */
+create table tournSmall AS
+SELECT t.id AS Tournament_Id, t.slug AS Tournament_Slug, et.name AS Match_Name, et2.id AS Matches_Ids, et3.id AS Game_Ids
+FROM tournaments t 
+LATERAL VIEW explode(stages.sections) exploaded_table AS et
+LATERAL VIEW explode(et.matches) exptab2 AS et2
+LATERAL VIEW explode(et2.games) exptab3 AS et3
